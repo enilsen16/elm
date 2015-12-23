28772d70adc445f77f7fafc0144586120253b14e
Erik Nilsen <enilsen16@live.com>
2015-12-23 13:57:01 -0800

    collage

---
+++ collage.elm
@@ -0,0 +1,7 @@
+import Text exposing (..)
+import Color exposing (..)
+import Graphics.Element exposing (..)
+import Graphics.Collage exposing (..)
+
+main =
+  collage 200 200 [filled blue (square 100) ]
