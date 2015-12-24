b94a9bf664a40e76f829ffa4350f8c8dc731d94e
Erik Nilsen <enilsen16@live.com>
2015-12-23 16:25:13 -0800

    diamond

--- diamond.elm
+++ diamond.elm
@@ -3,5 +3,8 @@ import Color exposing (..)
 import Graphics.Element exposing (..)
 import Graphics.Collage exposing (..)
 
+diamond color size =
+  rotate (degrees 45) (filled color (square size))
+
 main =
-  collage 200 200 [ rotate (degrees 45) (filled blue (square 100)) ]
+  collage 200 200 [ diamond red 100, diamond green 75 ]
