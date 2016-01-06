170ecfe74099e97469fa8bd13abb166bab285b52
Erik Nilsen <enilsen16@live.com>
2016-01-05 16:30:18 -0800

    add model

--- phoenix_elm/web/elm/SeatSaver.elm
+++ phoenix_elm/web/elm/SeatSaver.elm
@@ -1,8 +1,39 @@
 module SeatSaver where
-import Html
+import Html exposing (..)
+import Html.Attributes exposing (class)
 
+main : Html
 main =
-  view
+  view init
 
-view =
-  Html.text "Woo hoo, I'm in a View"
+type alias Seat =
+  { seatNo : Int
+  , occupied : Bool
+  }
+
+type alias Model =
+  List Seat
+
+init : Model
+init =
+  [ { seatNo = 1, occupied = False }
+  , { seatNo = 2, occupied = False }
+  , { seatNo = 3, occupied = False }
+  , { seatNo = 4, occupied = False }
+  , { seatNo = 5, occupied = False }
+  , { seatNo = 6, occupied = False }
+  , { seatNo = 7, occupied = False }
+  , { seatNo = 8, occupied = False }
+  , { seatNo = 9, occupied = False }
+  , { seatNo = 10, occupied = False }
+  , { seatNo = 11, occupied = False }
+  , { seatNo = 12, occupied = False }
+  ]
+
+view : Model -> Html
+view model =
+  ul [ class "seats" ] (List.map seatItem model)
+
+seatItem : Seat -> Html
+seatItem seat =
+  li [ class "seat available" ] [ text (toString seat.seatNo) ]
