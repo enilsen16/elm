d9969265e20f22312baa57cf1b21ba968c59cb37
Erik Nilsen <enilsen16@live.com>
2016-01-20 17:17:35 -0800

    get rid of folders

---
+++ elm-by-example/DelayedCircles.elm
@@ -0,0 +1,14 @@
+module DelayedCircles (..) where
+
+import DelayedMousePositions exposing (delayedMousePositions)
+import DrawCircles exposing (drawCircles)
+import Fibonacci exposing (fibonacci)
+import List exposing (drop, reverse)
+import Signal.Extra exposing ((~), (<~))
+import Window
+
+
+main =
+    drawCircles
+        <~ delayedMousePositions (fibonacci 8 |> drop 1 |> reverse)
+        ~ Window.dimensions
